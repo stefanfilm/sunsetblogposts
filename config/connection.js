ee18747f23903afaa52f2faa143ba7006d869ab7
Stefan Wanigatunga <stefan.wanigatunga@gmail.com>
2024-08-07 15:05:54 -0700

    deletes node modules

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+
+
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
